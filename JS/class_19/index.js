6ca31ccacd2cb9ca6d75088c1e7d71273f534786
Wesley Boaes <wesleyboaes@gmail.com>
2025-11-04 13:53:27 -0300

    Organizing Class 17 and 18

---
+++ JS/class_19/index.js
@@ -0,0 +1,5 @@
+/**
+ * Primitives (unchangeable) - strings, number, boolean, undefined, null (bigint, symbol) - Value
+ *
+ * References (changeable) - array, object, function - Passed by reference
+ */
