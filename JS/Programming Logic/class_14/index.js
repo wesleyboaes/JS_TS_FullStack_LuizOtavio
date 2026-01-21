e5527882412bb0df7b33ff9621f9b30c9aef946f
Wesley Pimenta Boaes <wesleyboaes@gmail.com>
2026-01-21 16:11:42 -0300

    Getting back to JS and TS

--- JS/Programming Logic/class_14/index.js
+++ JS/Programming Logic/class_14/index.js
@@ -2,3 +2,6 @@ for (let i = 0; i < 11; i++) {
   const even = i % 2 === 0 ? "even" : "odd";
   console.log(`Line ${i} is ${even}`);
 }
+
+const fruits = ["Apple", "Peach", "Grape"];
+console.log(fruits[1]);
